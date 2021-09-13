282248b2a02e9f986815b4ed95bab002f58fc9cb
Denis Braud <denisbraud@gmail.com>
2021-09-14 00:02:47 +0200

    add 2010-AfterAllTheStoryGoes

---
+++ docs/2010-AfterAllTheStoryGoes.js
@@ -0,0 +1,15 @@
+var trackCurrentIndex = 0;
+var tracks = [
+  {"title": "Here we are",               "file":"MuffinsZebulon-2010-AfterAllTheStoryGoes-01-HereWeAre.mp3"},
+  {"title": "Long ago",                  "file":"MuffinsZebulon-2010-AfterAllTheStoryGoes-02-LongAgo.mp3"},
+  {"title": "My Dad",                    "file":"MuffinsZebulon-2010-AfterAllTheStoryGoes-03-MyDad.mp3"},
+  {"title": "You'll be part of my world","file":"MuffinsZebulon-2010-AfterAllTheStoryGoes-04-YoullBePartOfMyWorld.mp3"},
+  {"title": "Typical postcard",          "file":"MuffinsZebulon-2010-AfterAllTheStoryGoes-05-TypicalPostcard.mp3"},
+  {"title": "I had enough",              "file":"MuffinsZebulon-2010-AfterAllTheStoryGoes-06-IHadEnough.mp3"},
+  {"title": "Many years from now",       "file":"MuffinsZebulon-2010-AfterAllTheStoryGoes-07-ManyYearsFromNow.mp3"},
+  {"title": "Precious one",              "file":"MuffinsZebulon-2010-AfterAllTheStoryGoes-08-PreciousOne.mp3"},
+  {"title": "Want it now",               "file":"MuffinsZebulon-2010-AfterAllTheStoryGoes-09-WantItNow.mp3"},
+  {"title": "Such a mess",               "file":"MuffinsZebulon-2010-AfterAllTheStoryGoes-10-SuchAMess.mp3"},
+  {"title": "Travel",                    "file":"MuffinsZebulon-2010-AfterAllTheStoryGoes-11-Travel.mp3"},
+  {"title": "After all the story goes",  "file":"MuffinsZebulon-2010-AfterAllTheStoryGoes-12-AfterAllTheStoryGoes.mp3"}
+];

a44e22d359b1218d6ae721d7d7f315b549f06a68
Denis Braud <denisbraud@gmail.com>
2021-09-14 00:06:48 +0200

    add 2000-MuffinsZebulon

---
+++ docs/2000-MuffinsZebulon.js
@@ -0,0 +1,14 @@
+var trackCurrentIndex = 0;
+var tracks = [
+  {"title": "Thank you to be here", "file":"MuffinsZebulon-2021-TimesRoll-01-TimesGoinBy.mp3"},
+  {"title": "King for a day",       "file":"MuffinsZebulon-2021-TimesRoll-02-GirlAndBoy.mp3"},
+  {"title": "Bitterness",           "file":"MuffinsZebulon-2021-TimesRoll-03-Vinaigre.mp3"},
+  {"title": "When we get old",      "file":"MuffinsZebulon-2021-TimesRoll-04-IllBeHere.mp3"},
+  {"title": "Lucky break",          "file":"MuffinsZebulon-2021-TimesRoll-05-OneMoreYear.mp3"},
+  {"title": "Rails of your life",   "file":"MuffinsZebulon-2021-TimesRoll-06-ItsTimeForYou.mp3"},
+  {"title": "Second floor",         "file":"MuffinsZebulon-2021-TimesRoll-07-Virus.mp3"},
+  {"title": "When you find love",   "file":"MuffinsZebulon-2021-TimesRoll-08-Alien.mp3"},
+  {"title": "It doesn't begin easy","file":"MuffinsZebulon-2021-TimesRoll-09-BadJoke.mp3"},
+  {"title": "You have come",        "file":"MuffinsZebulon-2021-TimesRoll-10-MysteryGirl.mp3"},
+  {"title": "Don't ever stay alone","file":"MuffinsZebulon-2021-TimesRoll-11-AVeryGoodMum.mp3"}
+];

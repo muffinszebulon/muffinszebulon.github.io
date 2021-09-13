80d4b39dd8d4c18c99f2553f0a5ec8bcd242be70
Denis Braud <denisbraud@gmail.com>
2021-09-13 23:36:36 +0200

    add 2021-TimesRoll

---
+++ docs/2021-TimesRoll.js
@@ -0,0 +1,14 @@
+var trackCurrentIndex = 0;
+var tracks = [
+  {"title": "Time's goin by",    "file":"MuffinsZebulon-2021-TimesRoll-01-TimesGoinBy.mp3"},
+  {"title": "Girl and boy",      "file":"MuffinsZebulon-2021-TimesRoll-02-GirlAndBoy.mp3"},
+  {"title": "Vinaigre",          "file":"MuffinsZebulon-2021-TimesRoll-03-Vinaigre.mp3"},
+  {"title": "I'll be here",      "file":"MuffinsZebulon-2021-TimesRoll-04-IllBeHere.mp3"},
+  {"title": "One more year",     "file":"MuffinsZebulon-2021-TimesRoll-05-OneMoreYear.mp3"},
+  {"title": "It's time for you", "file":"MuffinsZebulon-2021-TimesRoll-06-ItsTimeForYou.mp3"},
+  {"title": "Virus",             "file":"MuffinsZebulon-2021-TimesRoll-07-Virus.mp3"},
+  {"title": "Alien",             "file":"MuffinsZebulon-2021-TimesRoll-08-Alien.mp3"},
+  {"title": "Bad joke",          "file":"MuffinsZebulon-2021-TimesRoll-09-BadJoke.mp3"},
+  {"title": "Mystery girl",      "file":"MuffinsZebulon-2021-TimesRoll-10-MysteryGirl.mp3"},
+  {"title": "A very good mum",   "file":"MuffinsZebulon-2021-TimesRoll-11-AVeryGoodMum.mp3"}
+];

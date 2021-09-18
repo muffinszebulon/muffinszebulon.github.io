74a52a094258a6768bef4524fd0adc37a3e0f249
Denis Braud <denisbraud@gmail.com>
2021-09-19 00:20:38 +0200

    service-worker

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,104 +1,109 @@
-// src https://web.dev/offline-fallback-page/
-/*
-Copyright 2015, 2019, 2020, 2021 Google LLC. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
+/**
+ * ServiceWorker.<br>
+ * https://web.dev/offline-fallback-page/<br>
+ * https://gist.github.com/adactio/fbaa3a5952774553f5e7<br>
+ * https://googlechrome.github.io/samples/service-worker/basic/<br>
+ */
+const VERSION = '1';
+const CACHE_STATIC = `CACHE_STATIC_${VERSION}`;
+const CACHE_DYNAMIC = `CACHE_DYNAMIC_${VERSION}`;
+const CACHES = [CACHE_STATIC, CACHE_DYNAMIC];
+const OFFLINE_URL = 'offline.html';
 
-// Incrementing OFFLINE_VERSION will kick off the install event and force
-// previously cached resources to be updated from the network.
-// This variable is intentionally declared and unused.
-// Add a comment for your linter if you want:
-// eslint-disable-next-line no-unused-vars
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = "offline";
-// Customize this with a different URL if needed.
-const OFFLINE_URL = "offline.html";
-
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      // Setting {cache: 'reload'} in the new request will ensure that the
-      // response isn't fulfilled from the HTTP cache; i.e., it will be from
-      // the network.
-      await cache.addAll([
-        new Request(OFFLINE_URL, { cache: "reload" }),
-        new Request("favicon-32x32.png", { cache: "reload" }),
-        new Request("images/MuffinsZebulon-2021-TimesRoll_192.png", { cache: "reload" }),
-        new Request("images/MuffinsZebulon-2010-AfterAllTheStoryGoes_192.jpg", { cache: "reload" }),
-        new Request("images/MuffinsZebulon-2000-MuffinsZebulon_192.jpg", { cache: "reload" }),
-        
-        new Request("index.html", { cache: "reload" }),
-        new Request("2021-TimesRoll.html", { cache: "reload" }),
-        new Request("2010-AfterAllTheStoryGoes.html", { cache: "reload" }),
-        new Request("2000-MuffinsZebulon.html", { cache: "reload" }),
-        
-        new Request("audio/MuffinsZebulon-2021-TimesRoll-01-TimesGoinBy.mp3", { cache: "reload" }),
-        new Request("audio/MuffinsZebulon-2021-TimesRoll-02-Vinaigre.mp3", { cache: "reload" }),
-      ]);
-    })()
+      try {
+        const cache = await caches.open(CACHE_STATIC);
+        // Setting {cache: 'reload'} in the new request will ensure that
+        // the response isn't fulfilled from the HTTP cache;
+        // i.e., it will be from the network.
+        await cache.addAll([
+          new Request(OFFLINE_URL, { cache: 'reload' }),
+          new Request('favicon-32x32.png', { cache: 'reload' }),
+          new Request('images/MuffinsZebulon-header.jps', { cache: 'reload' }),
+          new Request('images/MuffinsZebulon-2021-TimesRoll_192.png', { cache: 'reload' }),
+          new Request('images/MuffinsZebulon-2010-AfterAllTheStoryGoes_192.jpg', { cache: 'reload' }),
+          new Request('images/MuffinsZebulon-2000-MuffinsZebulon_192.jpg', { cache: 'reload' }),
+  
+          new Request('2000-MuffinsZebulon.css', { cache: 'reload' }),
+          new Request('2000-MuffinsZebulon.html', { cache: 'reload' }),
+          new Request('2000-MuffinsZebulon.js', { cache: 'reload' }),
+          new Request('2010-AfterAllTheStoryGoes.css', { cache: 'reload' }),
+          new Request('2010-AfterAllTheStoryGoes.html', { cache: 'reload' }),
+          new Request('2010-AfterAllTheStoryGoes.js', { cache: 'reload' }),
+          new Request('2021-TimesRoll.css', { cache: 'reload' }),
+          new Request('2021-TimesRoll.html', { cache: 'reload' }),
+          new Request('2021-TimesRoll.js', { cache: 'reload' }),
+          new Request('audio.js', { cache: 'reload' }),
+          new Request('common-album.css', { cache: 'reload' }),
+          new Request('common.css', { cache: 'reload' }),
+          new Request('index.css', { cache: 'reload' }),
+          new Request('index.html', { cache: 'reload' }),
+          new Request('share.js', { cache: 'reload' }),
+  
+          new Request('audio/MuffinsZebulon-2021-TimesRoll-01-TimesGoinBy.mp3', { cache: 'reload' }),
+          new Request('audio/MuffinsZebulon-2021-TimesRoll-02-Vinaigre.mp3', { cache: 'reload' }),
+          new Request('audio/MuffinsZebulon-2021-TimesRoll-01-TimesGoinBy.mp3', { cache: 'reload' }),
+          new Request('audio/MuffinsZebulon-2021-TimesRoll-02-Vinaigre.mp3', { cache: 'reload' }),
+          new Request('audio/MuffinsZebulon-2021-TimesRoll-03-GirlAndBoy.mp3', { cache: 'reload' }),
+          new Request('audio/MuffinsZebulon-2021-TimesRoll-04-IllBeHere.mp3', { cache: 'reload' }),
+          new Request('audio/MuffinsZebulon-2021-TimesRoll-05-OneMoreYear.mp3', { cache: 'reload' }),
+          new Request('audio/MuffinsZebulon-2021-TimesRoll-06-ItsTimeForYou.mp3', { cache: 'reload' }),
+          new Request('audio/MuffinsZebulon-2021-TimesRoll-07-Virus.mp3', { cache: 'reload' }),
+          new Request('audio/MuffinsZebulon-2021-TimesRoll-08-Alien.mp3', { cache: 'reload' }),
+          new Request('audio/MuffinsZebulon-2021-TimesRoll-09-BadJoke.mp3', { cache: 'reload' }),
+          new Request('audio/MuffinsZebulon-2021-TimesRoll-10-MysteryGirl.mp3', { cache: 'reload' }),
+          new Request('audio/MuffinsZebulon-2021-TimesRoll-11-AVeryGoodMum.mp3', { cache: 'reload' }),
+        ]);
+      } catch (error) {
+        console.log(error);
+      }
+    })(),
   );
   // Force the waiting service worker to become the active service worker.
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
     (async () => {
-      // Enable navigation preload if it's supported.
-      // See https://developers.google.com/web/updates/2017/02/navigation-preload
-      if ("navigationPreload" in self.registration) {
-        await self.registration.navigationPreload.enable();
+      try {
+        const keys = await caches.keys();
+        for (let key of keys) {
+          if (CACHES.includes(key)) {
+            await caches.delete(key);
+          }
+        }
+      } catch (error) {
+        console.log(error);
       }
     })()
   );
-
   // Tell the active service worker to take control of the page immediately.
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      (async () => {
-        try {
-          // First, try to use the navigation preload response if it's supported.
-          const preloadResponse = await event.preloadResponse;
-          if (preloadResponse) {
-            return preloadResponse;
-          }
-
-          // Always try the network first.
-          const networkResponse = await fetch(event.request);
-          return networkResponse;
-        } catch (error) {
-          // catch is only triggered if an exception is thrown, which is likely
-          // due to a network error.
-          // If fetch() returns a valid HTTP response with a response code in
-          // the 4xx or 5xx range, the catch() will NOT be called.
-          console.log("Fetch failed; returning offline page instead.", error);
-
-          const cache = await caches.open(CACHE_NAME);
-          const cachedResponse = await cache.match(OFFLINE_URL);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    (async () => {
+      try {
+        const request = event.request;
+        if (request.method !== 'GET') { // Always fetch non-GET requests from the network
+          return fetch(request); // networkResponse
+        }
+        const cachedResponse = await caches.match(request);
+        if (cachedResponse) {
           return cachedResponse;
         }
-      })()
-    );
-  }
-
-  // If our if() condition is false, then this fetch handler won't intercept the
-  // request. If there are any other fetch handlers registered, they will get a
-  // chance to call event.respondWith(). If no fetch handlers call
-  // event.respondWith(), the request will be handled by the browser as if there
-  // were no service worker involvement.
+        const networkResponse = await fetch(request);
+        const cache = await caches.open(CACHE_DYNAMIC);
+        await cache.put(request, networkResponse.clone());
+        return networkResponse;
+      } catch (error) {
+        console.log(error);
+        return caches.match(OFFLINE_URL); // cachedResponse
+      }
+    })()
+  );
 });
